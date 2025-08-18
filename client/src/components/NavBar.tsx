3b50bf2011c9c6e64a7c10ae2c4b8a4901ae2de8
joshL1215 <210241055+joshL1215@users.noreply.github.com>
2025-08-17 23:58:34 -0700

    Add placeholder nav buttons

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -3,11 +3,15 @@ import siteLogo from '../assets/site-logo.svg';
 function NavBar() {
     return (
         <nav className="flex flex-row justify-between items-center px-5 bg-[#DCE0D9]">
+
+            {/* Logo */}
             <img className="max-h-20" src={siteLogo} />
-            <div className='flex flex-row'>
-                hi
-                hi
-                hi
+
+            {/* Nav Buttons */}
+            <div className='flex flex-row gap-5'>
+                <a href="/workspace">Home</a>
+                <a href="/workspace">Code</a>
+                <a href="/workspace">Settings</a>
 
             </div>
         </nav>
