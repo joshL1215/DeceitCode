662b4472ebeae2499f1205bf27f60cecbba5bba4
joshL1215 <210241055+joshL1215@users.noreply.github.com>
2025-08-20 02:49:47 -0700

    Implement language redux state change to dropdown

--- client/src/pages/Workspace.tsx
+++ client/src/pages/Workspace.tsx
@@ -1,14 +1,25 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import TextEditor from "../components/TextEditor";
 
-const LEFT_PANEL_STYLE = "flex flex-col items-center bg-[#DCE0D9] rounded-lg shadow overflow-hidden";
-const RIGHT_PANEL_STYLE = "flex flex-col items-center bg-[#DCE0D9] rounded-lg shadow overflow-hidden";
-const NAV_STYLE = "flex bg-[#DCE0D9] shadow w-full overflow-hidden"
+import { useDispatch, useSelector } from "react-redux";
+import type { RootState } from "../state/store.js"
+import { changeLanguage, changeProblem } from "../state/problem/problemSlice";
+
+const LEFT_PANEL_STYLE = "flex flex-col items-center bg-[#e0d9d9ff] rounded-lg shadow overflow-hidden";
+const RIGHT_PANEL_STYLE = "flex flex-col items-center bg-[#e0d9d9ff] rounded-lg shadow overflow-hidden";
+const NAV_STYLE = "flex bg-[#e0d9d9ff] shadow w-full overflow-hidden"
 const BUTTON_STYLE = "flex-1 h-full w-auto px-4"
 
 function Workspace() {
     const [leftPanelTab, setLeftPanelTab] = useState("Problem");
 
+    {/* Redux */ }
+    const currProblemID = useSelector((state: RootState) => state.problem.problemID);
+    const currLanguage = useSelector((state: RootState) => state.problem.language);
+    const dispatch = useDispatch();
+
+
+    {/* Dynamic button styling */ }
     const getFullStyle = (thisTab: string) => `${BUTTON_STYLE} ${thisTab === leftPanelTab ? "text-black font-semibold" : "hover:text-[#aa8c8c]"}`
 
     return (
@@ -39,20 +50,21 @@ function Workspace() {
 
             {/* Editor panel */}
             <div className={`${RIGHT_PANEL_STYLE} flex-3`}>
-                <div className={`${NAV_STYLE} h-10 flex items-center justify-between p-4 font-semibold`}>
+                <div className={`${NAV_STYLE} h-10 flex items-center justify-between p-4 `}>
                     <div className={'flex flex-row gap-5'}>
-                        <p>Code</p>
-                        <select className="focus:outline-none">
-                            <option value="option1">Python</option>
-                            <option value="option2">JavaScript</option>
-                            <option value="option3">C++</option>
+                        <p className="font-semibold">Code</p>
+                        <select className="focus:outline-none" onChange={(e: React.ChangeEvent<HTMLSelectElement>) => dispatch(changeLanguage(e.target.value))}>
+                            <option value="python">Python</option>
+                            <option value="javascript">JavaScript</option>
+                            <option value="cpp">C++</option>
                         </select>
-                    </div>
 
+                        {/* <h2>{currLanguage}</h2> TODO: delete, for testing redux language state  */}
+
+                    </div>
                     <button>
                         Reset
                     </button>
-
                 </div>
                 <div className={'flex-1 w-full py-2'}>
                     <TextEditor language='python' starterCode='print(hello world)' />
