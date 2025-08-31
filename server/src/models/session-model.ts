68feff7a896a08a46957fbf9340b68df81f847c9
joshL1215 <210241055+joshL1215@users.noreply.github.com>
2025-08-31 15:02:13 -0700

    Add session creation on signup

---
+++ server/src/models/session-model.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+export interface SessionDoc extends mongoose.Document {
+    userId: mongoose.Types.ObjectId;
+    userAgent?: string;
+    createdAt: Date;
+    expiresAt: Date; 
+}
+
+const sessionSchema = new mongoose.Schema<SessionDoc>({
+    userId: {
+        ref: "User",
+        type: mongoose.Schema.Types.ObjectId,
+        index: true,
+    },
+    userAgent: {type : String},
+    createdAt: {type: Date, required: true, default: Date.now},
+    expiresAt: {type: Date, default: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000)}
+});
+
+export default mongoose.model<SessionDoc>("User", sessionSchema);
--- server/src/services/auth-service.ts
+++ server/src/services/auth-service.ts
@@ -1,3 +1,4 @@
+import SessionModel from "../models/session-model";
 import UserModel from "../models/user-model";
 
 export type CreateAccountParams = {
@@ -7,7 +8,7 @@ export type CreateAccountParams = {
 }
 export const createAccount = async (data: CreateAccountParams) => {
     if (await UserModel.exists({username: data.username})) {
-        throw new Error("An account with this email already exists")
+        throw new Error("An account with this username already exists")
     }
 
     const user = await UserModel.create({
@@ -15,5 +16,11 @@ export const createAccount = async (data: CreateAccountParams) => {
         password: data.password,
     });
 
+    const sesson = await SessionModel.create({
+        userId: user._id,
+        userAgent: data.userAgent,
+    });
+
+    
 
 }
