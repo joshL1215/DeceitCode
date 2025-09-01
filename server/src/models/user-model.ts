cee78e86c02d6e1d65e1aef0f8a1d9a9e3fc4846
joshL1215 <210241055+joshL1215@users.noreply.github.com>
2025-08-31 18:37:15 -0700

    Fix tiny conditional issue leading to no password hash

--- server/src/models/user-model.ts
+++ server/src/models/user-model.ts
@@ -20,7 +20,7 @@ const userSchema = new mongoose.Schema<UserDoc>({
 );
 
 userSchema.pre("save", async function (next){ 
-    if (this.isModified("password")) {
+    if (!this.isModified("password")) {
         return;
     }
     const saltRounds = 10;
