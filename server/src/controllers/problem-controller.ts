25004f1aca5f41ef6f421655fe02100b9d608d5e
joshL1215 <210241055+joshL1215@users.noreply.github.com>
2025-09-04 17:36:04 -0700

    Add prelim and test case enum to judge schema and payload

--- server/src/controllers/problem-controller.ts
+++ server/src/controllers/problem-controller.ts
@@ -19,3 +19,42 @@ export const fetchOneHandler = catchErrors(
     }
 )
 
+const codeSubmissionSchema = z.object({
+    slug: z.string().min(1).max(256),
+    language: z.string().min(1).max(32),
+    code: z.string().min(1).max(10000),
+    mode: z.enum(["prelim","test"]),
+    }
+);
+
+export const judgeHandler = catchErrors(
+    async (req, res) => {
+        const request = codeSubmissionSchema.parse(req.body);
+        const problem = await fetchProb(request.slug);
+
+        const cases = request.mode === "prelim" ? problem.prelimCases : problem.testCases;
+
+        const payload = {
+            language: "python",
+            code: "print('hi')",
+            cases,
+        }
+
+        const response = await fetch('http://localhost:5001/', {
+            method: 'POST',
+            headers: {
+                'Content-Type' : 'application/json',
+            },
+            body: JSON.stringify(payload),
+        });
+
+
+        // get the problem test cases using the slug (only send)
+        // send everything through to microservice
+        // get results
+        // eval
+        //send to frontend
+
+    }
+)
+
