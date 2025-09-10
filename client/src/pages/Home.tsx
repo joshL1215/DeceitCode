785628e1ed14bc2df253b8196914f25f2cf53d9a
joshL1215 <210241055+joshL1215@users.noreply.github.com>
2025-09-10 12:11:08 -0700

    Change intro message on homepage

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,13 +2,12 @@ import siteLogo from "../assets/site-logo.svg"
 function Home() {
 
     return (
-        <div className="flex flex-col items-center pt-10 gap-7 h-screen bg-[#f9f8f8]">
+        <div className="flex flex-col items-center justify-center pb-20 gap-5 h-screen bg-[#f9f8f8]">
             <div>
                 <img className="max-h-75" src={siteLogo} />
             </div>
             <h1 className="text-xl font-semibold">A coding challenge platform built to deter AI-based cheating tools</h1>
-            <text className="whitespace-pre-wrap max-w-200 text-center">{
-                `I built this app to play around with some ideas I had on how cheating with AI/LLM powered tools could be prevented during online coding assessments.`}</text>
+            <div className="text-center whitespace-pre-wrap">{`I built this app to practice and learn technologies while also experimenting with an interesting idea:\nHow can cheating with LLM powered tools be better prevented during coding assessments?`}</div>
         </div>
     );
 
